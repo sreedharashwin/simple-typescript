891f163f5acf3bb457c9fcf489073b30c345828e
Ashwin Sreedhar <150316253+sreedharashwin@users.noreply.github.com>
2025-01-03 22:56:21 +1100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { sum } from './example/utils';
 
-console.log('Hello TypeScript! This is starting poing of app!');
+console.log('Hello TypeScript! This is starting point of app!');
 
-console.log(sum(2, 2));
+console.log("The sume is: "+sum(2, 2));
+
+console.log("The result is: "+multiply(10,6));
+
+console.log("Division: "+divide(100,4));
